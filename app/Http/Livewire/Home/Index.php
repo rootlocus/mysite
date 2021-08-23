1f4bbb7dae1ad5cbd43c106533f89d6c941d47ff
rootlocus <erickokkuan@gmail.com>
2021-08-24 01:29:46 +0800

    added timeline

--- app/Http/Livewire/Home/Index.php
+++ app/Http/Livewire/Home/Index.php
@@ -6,6 +6,13 @@ use Livewire\Component;
 
 class Index extends Component
 {
+    public $years;
+
+    public function mount()
+    {
+        $this->years = now()->year - 2014;
+    }
+
     public function render()
     {
         return view('livewire.home.index');
--- resources/views/livewire/home/index.blade.php
+++ resources/views/livewire/home/index.blade.php
@@ -1,5 +1,20 @@
-<div class="bg-red-500">
-    <h1 class="red">
-        THIS IS HOMEzz
-    </h1>
+<div class="bg-black h-full">
+    <div class="flex justify-center items-center h-screen">
+        <img src="{{ asset('storage/images/duck.png')}}" style="height: 150px;"/>
+        <h1 class="text-white font-bold text-8xl ">
+            ERIC HENG
+        </h1>
+
+    </div>
+    <div class="flex flex-col justify-center items-start  p-40">
+        <div class="text-white">
+            <u class="flex justify-center">Backend Developer</u>
+            <p class="text-xl">
+                Ready for new challenges and finding new ways to level up. 
+                I enjoy working in a team environment where teamwork and communication skills are key. 
+                Been a programmer since 2014 which is about {{ $years }} years.
+            </p>
+        </div>
+        <x-timeline />
+    </div>
 </div>
