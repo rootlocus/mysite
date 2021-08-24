882877e2df6ab798c0fcf7b0e56f078ace756d77
rootlocus <erickokkuan@gmail.com>
2021-08-24 12:35:24 +0800

    cleanup details

--- resources/views/components/timeline.blade.php
+++ resources/views/components/timeline.blade.php
@@ -8,22 +8,22 @@
         <!-- right timeline -->
         <div class="mb-8 flex justify-between items-center w-full right-timeline">
             <div class="order-1 w-5/12"></div>
-            <div class="z-20 flex items-center order-1 bg-gray-800 shadow-xl rounded-lg p-1">
+            <div class="z-20 flex items-center order-1 bg-green-500 shadow-xl rounded-lg p-1">
                 <h1 class="mx-auto font-semibold text-lg text-white">2014 - 2017</h1>
             </div>
-            <div class="order-1 bg-white rounded-lg shadow-xl w-5/12 px-6 py-4">
-                <h3 class="mb-1 font-bold text-blue-800 text-xl">DXC Technologies</h3>
-                <p class="text-sm leading-snug tracking-wide text-blue-900 text-opacity-100 underline">
+            <div class="order-1 bg-indigo-800 rounded-lg shadow-xl w-5/12 px-6 py-4">
+                <h3 class="mb-1 font-bold text-white text-xl hover:underline"><a href="https://www.dxc.com/my/en" target="_blank">DXC Technology (Previously known as CSC)</a></h3>
+                <p class="text-sm leading-snug tracking-wide text-white text-opacity-100 underline">
                     <b>Tools:</b> AS400, SMART400, CLLE, CL/400, IBM DB2//400 relational database,
                 </p>
-                <ul class="list-disc text-sm leading-snug tracking-wide text-blue-900 text-opacity-100">
+                <ul class="list-disc text-sm leading-snug tracking-wide text-white text-opacity-100">
                     <li>Data migration to new GROUP/400 system.</li>
                     <li>Develop new and enhance existing programs.</li>
                     <li>Create & enhance business objects, screens, reports, table programs, batch programs</li>
-                    <li>Has worked along side with clients in UAT closely to achieve requirements.</li>
-                    <li>Has done impact analysis and preparing documentation for change requests.</li>
-                    <li>Has performed data analysis study to fix user’s corrupted data by data patching.</li>
-                    <li>Has traveled overseas for other projects which involved Data Migration.</li>
+                    <li>Worked along side with clients in UAT closely to achieve requirements.</li>
+                    <li>Impact analysis and preparing documentation for change requests.</li>
+                    <li>Performed data analysis study to fix user’s corrupted data by data patching.</li>
+                    <li>Traveled overseas for other projects which involved Data Migration.</li>
                 </ul>
             </div>
         </div>
@@ -31,34 +31,35 @@
         <!-- left timeline -->
         <div class="mb-8 flex justify-between flex-row-reverse items-center w-full left-timeline">
             <div class="order-1 w-5/12"></div>
-            <div class="z-20 flex items-center order-1 bg-gray-800 shadow-xl rounded-lg p-1">
+            <div class="z-20 flex items-center order-1 bg-green-500 shadow-xl rounded-lg p-1">
                 <h1 class="mx-auto text-white font-semibold text-lg">2017 - 2019</h1>
             </div>
-            <div class="order-1 bg-white rounded-lg shadow-xl w-5/12 px-6 py-4">
-                <h3 class="mb-1 font-bold text-blue-800 text-xl">Finatext Malaysia Sdn. Bhd.</h3>
-                <p class="text-sm leading-snug tracking-wide text-blue-900 text-opacity-100 underline">
+            <div class="order-1 bg-indigo-800 rounded-lg shadow-xl w-5/12 px-6 py-4">
+                <h3 class="mb-1 font-bold text-white text-xl hover:underline"><a href="https://finatext.com/" target="_blank" >Finatext Malaysia Sdn. Bhd.</a></h3>
+                <p class="text-sm leading-snug tracking-wide text-white text-opacity-100 underline">
                     <b>Tools:</b> Ruby On Rails, Google Analytics, Adjust, FB Pixels, Google Scripts, Zapier
                 </p>
-                <ul class="list-disc text-sm leading-snug tracking-wide text-blue-900 text-opacity-100">
+                <ul class="list-disc text-sm leading-snug tracking-wide text-white text-opacity-100">
                     <li>Requirement gathering and testing of Pipster mobile application.</li>
                     <li>Designing functional specifications for new mobile applications and change requests.</li>
                     <li>Setting up analytic tools for product.</li>
-                    <li>Has used google sheets for automation.</li>
-                    <li>Has helped to setup EC2 instance for host an existing web application.</li>
+                    <li>Used google scripts for automation.</li>
+                    <li>Has helped to setup EC2 instance to host an existing web application.</li>
+                    <li>Traveled to Vietnam to work along side developers.</li>
                 </ul>
             </div>
         </div>
         <div class="mb-8 flex justify-between items-center w-full right-timeline">
             <div class="order-1 w-5/12"></div>
-            <div class="z-20 flex items-center order-1 bg-gray-800 shadow-xl rounded-lg p-1">
+            <div class="z-20 flex items-center order-1 bg-green-500 shadow-xl rounded-lg p-1">
                 <h1 class="mx-auto font-semibold text-lg text-white">2019 - 2021</h1>
             </div>
-            <div class="order-1 bg-gray-400 rounded-lg shadow-xl w-5/12 px-6 py-4">
-                <h3 class="mb-1 font-bold text-blue-800 text-xl">Supplycart Technologies</h3>
-                <p class="text-sm leading-snug tracking-wide text-blue-900 text-opacity-100 underline">
+            <div class="order-1 bg-indigo-800 rounded-lg shadow-xl w-5/12 px-6 py-4">
+                <h3 class="mb-1 font-bold text-white text-xl hover:underline"><a href="https://www.supplycart.my/" target="_blank" >Supplycart Technologies Sdn. Bhd.</a></h3>
+                <p class="text-sm leading-snug tracking-wide text-white text-opacity-100 underline">
                     <b>Tools:</b> Laravel, Vue.Js, Tailwind, InertiaJs, MySQL, Postgres, Linux, PhpUnit
                 </p>
-                <ul class="list-disc text-sm leading-snug tracking-wide text-blue-900 text-opacity-100">
+                <ul class="list-disc text-sm leading-snug tracking-wide text-white text-opacity-100">
                     <li>Developed and improved procurement processes.</li>
                     <li>Worked on both internal tools and product website.</li>
                     <b class="underline">Internal</b>
