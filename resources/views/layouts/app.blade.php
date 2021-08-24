b2bace4b3a9cc2ccf88c70562755b08bc80466d7
rootlocus <erickokkuan@gmail.com>
2021-08-24 19:54:51 +0800

    added title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Thai&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Thai:wght@600&family=Ubuntu:wght@300&display=swap" rel="stylesheet">
+    <title>Eric Heng - Web Developer, PC Gamer & Magic Enthusiast</title>
     @livewireStyles
 </head>
 <body>
