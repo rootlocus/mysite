9630f37cd1a5b5ececb1983c83521403d65865f9
rootlocus <erickokkuan@gmail.com>
2021-08-24 13:53:01 +0800

    change bg color

--- resources/views/livewire/home/index.blade.php
+++ resources/views/livewire/home/index.blade.php
@@ -1,12 +1,12 @@
-<div class="bg-black h-full">
-    <div class="flex justify-center items-center h-screen">
+<div class="h-full">
+    <div class="flex justify-center items-center h-screen bg-black">
         <img src="{{ asset('storage/images/duck.png')}}" style="height: 150px;"/>
         <h1 class="text-white font-bold text-8xl ">
             ERIC HENG
         </h1>
 
     </div>
-    <div class="flex flex-col justify-center items-start px-40 pt-20">
+    <div class="flex flex-col justify-center items-start px-40 pt-20 bg-gray-900">
         <div class="text-white">
             <h2 class="flex justify-center font-bold text-2xl">Backend Developer</h2>
             <p class="text-xl">
