a59f566face8334d194c132678ef5e8a54f552ef
rootlocus <erickokkuan@gmail.com>
2021-08-25 22:26:24 +0800

    add local check

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,9 +13,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        if(config('app.env') === 'production') {
-            \URL::forceScheme('https');
-        }
+        // if(config('app.env') === 'local') {
+        //     \URL::forceScheme('http');
+        // }
     }
 
     /**
