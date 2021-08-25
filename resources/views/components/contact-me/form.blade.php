c4c064147fba14f1d5fb06bbb7877fdd1669d3d5
rootlocus <erickokkuan@gmail.com>
2021-08-25 21:37:36 +0800

    all pages fix mobile

--- resources/views/components/contact-me/form.blade.php
+++ resources/views/components/contact-me/form.blade.php
@@ -1,6 +1,6 @@
 <form class="w-full max-w-lg" wire:submit.prevent="submit">
     <div class="flex flex-wrap -mx-3 mb-6">
-      <div class="w-full px-3">
+      <div class="w-full px-8">
         <label class="block uppercase tracking-wide text-indigo-800 text-xs font-bold mb-2 content" for="name">
           Name *
         </label>
@@ -13,7 +13,7 @@
       </div>
     </div>
     <div class="flex flex-wrap -mx-3 mb-6">
-      <div class="w-full px-3">
+      <div class="w-full px-8">
         <label class="block uppercase tracking-wide text-indigo-800 text-xs font-bold mb-2 content" for="email">
           E-mail *
         </label>
@@ -26,7 +26,7 @@
       </div>
     </div>
     <div class="flex flex-wrap -mx-3 mb-6">
-      <div class="w-full px-3">
+      <div class="w-full px-8">
         <label class="block uppercase tracking-wide text-indigo-800 text-xs font-bold mb-2 content" for="message">
           Message *
         </label>
@@ -38,14 +38,13 @@
       </div>
     </div>
     <div class="md:flex md:items-center">
-      <div class="md:w-1/3">
+      <div class="px-4">
         <button 
-          class="shadow {{ $isDisabled ? 'text-indigo-400 cursor-not-allowed' :'text-indigo-800 hover:bg-indigo-500 focus:shadow-outline focus:outline-none'}} bg-indigo-300 font-bold py-2 px-4 rounded content"
+          class="w-full {{ $isDisabled ? 'text-indigo-400 cursor-not-allowed' :'shadow text-indigo-800 hover:bg-indigo-500 focus:shadow-outline focus:outline-none'}} bg-indigo-300 font-bold py-2 px-4 rounded content"
           type="submit"
           {{ $isDisabled ? 'disabled' : ''}}>
           Send
         </button>
       </div>
-      <div class="md:w-2/3"></div>
     </div>
   </form>
--- resources/views/components/timeline/section.blade.php
+++ resources/views/components/timeline/section.blade.php
@@ -1,4 +1,4 @@
-<div class="my-10 flex justify-between items-center w-full {{$direction}} pb-10">
+<div id="content-desktop" class="my-10 flex justify-between items-center w-full {{$direction}} pb-10">
     <div class="order-1 w-5/12"></div>
     <div class="z-20 flex items-center order-1 bg-green-500 shadow-xl rounded-lg p-1 filter drop-shadow-lg">
         <h1 class="mx-auto font-semibold text-lg text-white content">{{ $year }}</h1>
@@ -9,6 +9,20 @@
             <b class="text-l">Tools:</b> {{ $tools }}
         </p>
         <ul class="list-disc text-sm leading-snug tracking-wide text-white text-opacity-100 leading-relaxed">
+            {{ $slot }}
+        </ul>
+    </div>
+</div>
+<div id="content-mobile" class="my-10 flex justify-between items-center w-full {{$direction}} pb-10">
+    <div class="flex items-center order-1 bg-green-500 shadow-l rounded-lg filter drop-shadow-lg mb-2">
+        <h1 class="mx-auto font-semibold text-lg text-white content">{{ $year }}</h1>
+    </div>
+    <div class="order-1 bg-indigo-800 rounded-lg shadow-l px-8 py-4 filter drop-shadow-lg">
+        <h3 class="mb-1 font-bold text-white text-xl hover:underline hover:text-green-500 underline"><a href="{{$titleLink}}" target="_blank">{{ $title }}</a></h3>
+        <p class="text-xs leading-snug tracking-wide text-white text-opacity-100 py-2 font-mono">
+            <b class="text-l">Tools:</b> {{ $tools }}
+        </p>
+        <ul class="list-disc text-xs leading-snug tracking-wide text-white text-opacity-100 leading-relaxed">
             {{ $slot }}
         </ul>
     </div>
