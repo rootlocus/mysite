30c9371bccf1af32de210ecfccf7df4bbb7fb7b9
rootlocus <erickokkuan@gmail.com>
2021-08-27 19:23:22 +0800

    recenter gallery image

--- resources/views/components/about-me/gallery.blade.php
+++ resources/views/components/about-me/gallery.blade.php
@@ -1,8 +1,8 @@
 <div class="mx-auto max-w-2xl">
     {{-- <h2 class="text-4xl text-center tracking-wide font-extrabold font-serif leading-loose mb-2">Slideshow Gallery</h2> --}}
-    <div class="shadow-2xl relative">
+    <div class="flex flex-col justify-center items-center shadow-2xl relative">
         <!-- large image on slides -->
-          <div class="mySlides hidden" style="display: block;">
+          <div style="display: block;">
               <div class="w-full object-cover"></div>
               <div class="">
                 <img src="{{ local_images($currentImage) }}" src="#" class="max-h-96 md:max-h-128">
