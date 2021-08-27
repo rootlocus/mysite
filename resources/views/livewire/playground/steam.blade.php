deb872e2f1ee1e8b7e17831e5cb0710c7bc430b3
rootlocus <erickokkuan@gmail.com>
2021-08-28 02:53:34 +0800

    removed age

--- resources/views/livewire/playground/steam.blade.php
+++ resources/views/livewire/playground/steam.blade.php
@@ -9,12 +9,12 @@
             <div class="flex flex-row justify-center items-center content-start space-x-10 pt-4 w-full">
                 <div class="relative mr-6">
                     <input wire:model="steamId" type="search" class="bg-purple-white shadow rounded border-0 p-3" placeholder="Steam Id (eg; 76561197972495328)" style="width: 500px;">
-                    <input wire:model="age" type="search" class="bg-purple-white shadow rounded border-0 p-3" placeholder="Age" style="width: 100px;">
+                    {{-- <input wire:model="age" type="search" class="bg-purple-white shadow rounded border-0 p-3" placeholder="Age" style="width: 100px;"> --}}
                     <button wire:click="retrieve" class="bg-blue-400 hover:bg-blue-dark text-white font-bold px-4 py-2 rounded">Calculate</button>
                     <button wire:click="resetData" class="bg-blue-400 hover:bg-blue-dark text-white font-bold px-4 py-2 rounded">Reset</button>
                 </div>
             </div>
-            <a href="https://www.thegamer.com/how-to-find-your-steam-id/" class="content text-blue-500">How to get your steam id</a>
+            <a href="https://www.thegamer.com/how-to-find-your-steam-id/" class="content text-blue-500 text-xl">How to get your steam id ?</a>
         </div>
     </section>
 </div>
