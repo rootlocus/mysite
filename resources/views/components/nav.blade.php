2bc1d9030f5c05ae9f964097b667d8756f1c4876
rootlocus <erickokkuan@gmail.com>
2021-08-30 11:14:07 +0800

    put nav in component

---
+++ resources/views/components/nav.blade.php
@@ -0,0 +1,7 @@
+<nav class="absolute">
+    <div class="max-w-7xl text-gray-400 space-x-4 text-xl content font-semibold filter drop-shadow-md py-4 pl-6">
+        <a class="active" href="/">home</a>
+        <a href="/about-me">about</a>
+        <a href="/playground">playground</a>
+    </div>
+</nav>
