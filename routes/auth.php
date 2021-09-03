6cdb4b7464a587fc0219f531eef2e02cfc66b262
rootlocus <erickokkuan@gmail.com>
2021-09-03 17:41:22 +0800

    comment out routes

--- routes/auth.php
+++ routes/auth.php
@@ -34,38 +34,38 @@ Route::get('/dashboard', function () {
         return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
-Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])
-                ->middleware('guest')
-                ->name('password.email');
+// Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])
+//                 ->middleware('guest')
+//                 ->name('password.email');
 
-Route::get('/reset-password/{token}', [NewPasswordController::class, 'create'])
-                ->middleware('guest')
-                ->name('password.reset');
+// Route::get('/reset-password/{token}', [NewPasswordController::class, 'create'])
+//                 ->middleware('guest')
+//                 ->name('password.reset');
 
-Route::post('/reset-password', [NewPasswordController::class, 'store'])
-                ->middleware('guest')
-                ->name('password.update');
+// Route::post('/reset-password', [NewPasswordController::class, 'store'])
+//                 ->middleware('guest')
+//                 ->name('password.update');
 
-Route::get('/verify-email', [EmailVerificationPromptController::class, '__invoke'])
-                ->middleware('auth')
-                ->name('verification.notice');
+// Route::get('/verify-email', [EmailVerificationPromptController::class, '__invoke'])
+//                 ->middleware('auth')
+//                 ->name('verification.notice');
 
-Route::get('/verify-email/{id}/{hash}', [VerifyEmailController::class, '__invoke'])
-                ->middleware(['auth', 'signed', 'throttle:6,1'])
-                ->name('verification.verify');
+// Route::get('/verify-email/{id}/{hash}', [VerifyEmailController::class, '__invoke'])
+//                 ->middleware(['auth', 'signed', 'throttle:6,1'])
+//                 ->name('verification.verify');
 
-Route::post('/email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
-                ->middleware(['auth', 'throttle:6,1'])
-                ->name('verification.send');
+// Route::post('/email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
+//                 ->middleware(['auth', 'throttle:6,1'])
+//                 ->name('verification.send');
 
                 // add confirm page
-Route::get('/confirm-password', [ConfirmablePasswordController::class, 'show'])
-                ->middleware('auth')
-                ->name('password.confirm');
+// Route::get('/confirm-password', [ConfirmablePasswordController::class, 'show'])
+//                 ->middleware('auth')
+//                 ->name('password.confirm');
 
-Route::post('/confirm-password', [ConfirmablePasswordController::class, 'store'])
-                ->middleware('auth');
+// Route::post('/confirm-password', [ConfirmablePasswordController::class, 'store'])
+//                 ->middleware('auth');
 
-Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
-                ->middleware('auth')
-                ->name('logout');
+// Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
+//                 ->middleware('auth')
+//                 ->name('logout');
