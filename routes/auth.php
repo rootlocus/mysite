6ad7590c0546f45175d1c5ce7fd4d6ca9ba700bf
rootlocus <erickokkuan@gmail.com>
2021-09-06 00:08:01 +0800

    library cleanup

--- routes/auth.php
+++ routes/auth.php
@@ -1,10 +1,6 @@
 <?php
 
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
-use App\Http\Controllers\Auth\ConfirmablePasswordController;
-use App\Http\Controllers\Auth\EmailVerificationNotificationController;
-use App\Http\Controllers\Auth\EmailVerificationPromptController;
-use App\Http\Controllers\Auth\NewPasswordController;
 use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
